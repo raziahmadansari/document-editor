f486e78e79c2bb67c2f7abed28236005103f09bd
Razi Ahmad Ansari <raziahmad.abd@gmail.com>
2024-07-03 16:25:11 +0530

    added ckeditor module

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -2,19 +2,15 @@ import { NgModule } from '@angular/core';
 import { BrowserModule } from '@angular/platform-browser';
 
 import { AppRoutingModule } from './app-routing.module';
+import { CKEditorModule } from '@ckeditor/ckeditor5-angular';
+
 import { AppComponent } from './app.component';
 import { EditorComponent } from './editor/editor.component';
 
 @NgModule({
-  declarations: [
-    AppComponent,
-    EditorComponent
-  ],
-  imports: [
-    BrowserModule,
-    AppRoutingModule
-  ],
+  declarations: [AppComponent, EditorComponent],
+  imports: [BrowserModule, AppRoutingModule, CKEditorModule],
   providers: [],
-  bootstrap: [AppComponent]
+  bootstrap: [AppComponent],
 })
-export class AppModule { }
+export class AppModule {}
