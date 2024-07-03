abca8afb369af0e1fa4fc148175eada5d61151b2
Razi Ahmad Ansari <raziahmad.abd@gmail.com>
2024-07-03 17:04:39 +0530

    added editor constants

---
+++ src/app/models/editor.constant.ts
@@ -0,0 +1,12 @@
+/**
+ * Please update the following values with your actual tokens.
+ * Instructions on how to obtain them: https://ckeditor.com/docs/trial/latest/guides/real-time/quick-start.html
+ */
+export const LICENSE_KEY = '<YOUR_LICENSE_KEY>';
+export const AI_AUTH_TOKEN = '<YOUR_AI_AUTH_TOKEN>';
+export const AI_API_URL = '<YOUR_AI_API_URL>';
+export const CKBOX_TOKEN_URL = '<YOUR_CKBOX_TOKEN_URL>';
+export const UNIQUE_CHANNEL_PER_DOCUMENT = '<YOUR_UNIQUE_CHANNEL_PER_DOCUMENT>';
+export const CLOUD_SERVICES_TOKEN_URL = '<YOUR_CLOUD_SERVICES_TOKEN_URL>';
+export const CLOUD_SERVICES_WEBSOCKET_URL =
+  '<YOUR_CLOUD_SERVICES_WEBSOCKET_URL>';
